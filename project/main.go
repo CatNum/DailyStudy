58e6b09c8b12e8cbc7a238009e3e08ba0c97020e
CatNum <yxl19991104@gmail.com>
2023-07-19 19:42:08 +0800

    README.md:增加TODO

--- project/main.go
+++ project/main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	var a int
-	//fmt.Println(i, &i,*i)
-	// 输出： nil i的地址 *i报错
-	i := &a
-	fmt.Println(i, &i, *i)
-	*i = 10
-	fmt.Println(i, &i, *i)
-	// 输出： i存储的变量的地址 i的地址 i存储的变量的值10
-
+	
 }
