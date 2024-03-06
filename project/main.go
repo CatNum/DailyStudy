0e1ea991c482f486112f50ae2ff894ab7420d6c0
anini <1660470561@qq.com>
2024-03-06 18:47:08 +0800

    update：更新mysql笔记名称 && 添加 channel 和 select

--- project/main.go
+++ project/main.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	slice1 := []int{1, 2, 3}
+	change1(slice1)
+
+	fmt.Println("传递切片", slice1)
+
+	slice2 := []int{1, 2, 3}
+	change(&slice2)
+
+	fmt.Println("传递切片指针", slice2)
+}
+
+func change(aa *[]int) {
+	*aa = append(*aa, []int{1, 3, 4, 6, 5}...)
+}
+
+func change1(aa []int) {
+	aa = append(aa, []int{1, 3, 4, 6, 5}...)
 }
