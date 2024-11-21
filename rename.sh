32cb6c862ec41df8c166708f01c62dcf01062e1e
CatNum <yxl19991104@gmail.com>
2024-11-21 23:18:55 +0800

    update:增加注释

--- rename.sh
+++ rename.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+## 1. 执行rename.sh （windows 下可以在 git bash 里面执行）
+## 2. 通过 git log 查看是否已修改完毕
+## 3. 同步远程仓库 git push origin --force --all
+
 git filter-branch -f --env-filter '
 
 OLD_EMAIL="1660470561@qq.com"
