fdff15946eb15ab568a0d614877e3240a6d7faff
CatNum <yxl19991104@gmail.com>
2024-11-21 22:48:38 +0800

    add:添加更改提交的脚本

---
+++ rename.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="1660470561@qq.com"
+OLD_NAME="CatNum"
+CORRECT_NAME="CatNum"
+CORRECT_EMAIL="yxl19991104@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+
+if [ "GIT_COMMITTER_NAME" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "GIT_AUTHOR_NAME" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+
+' --tag-name-filter cat -- --branches --tags
