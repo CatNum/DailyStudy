f0d36d9427b93e55826e131421bd749fbbbe1269
CatNum <yxl19991104@gmail.com>
2024-11-22 20:55:39 +0800

    add:添加 gitignore，修改脚本

--- rename.sh
+++ rename.sh
@@ -6,18 +6,18 @@
 
 git filter-branch -f --env-filter '
 
-OLD_EMAIL="1660470561@qq.com"
-OLD_NAME="anini"
+# OLD_EMAIL=""
+# OLD_NAME="anini"
 CORRECT_NAME="CatNum"
 CORRECT_EMAIL="yxl19991104@gmail.com"
 
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ] || [ "$GIT_COMMITTER_NAME" = "$OLD_NAME" ]
+if [ "$GIT_COMMITTER_EMAIL" != "$CORRECT_NAME" ] || [ "$GIT_COMMITTER_NAME" != "$CORRECT_NAME" ]
 then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
 
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ] || [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ]
+if [ "$GIT_AUTHOR_EMAIL" != "$CORRECT_EMAIL" ] || [ "$GIT_AUTHOR_NAME" != "$CORRECT_EMAIL" ]
 then
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
